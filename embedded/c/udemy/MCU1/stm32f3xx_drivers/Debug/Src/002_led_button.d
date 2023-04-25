7fdbc4cd732cb3157b63bed4353c3ca44b5804c3
Maksym Solomodenko <maxim.solomodenko@gmail.com>
2023-04-25 17:58:10 +0300

    .

---
+++ embedded/c/udemy/MCU1/stm32f3xx_drivers/Debug/Src/002_led_button.d
@@ -0,0 +1,7 @@
+Src/002_led_button.o: ../Src/002_led_button.c \
+ /Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx.h \
+ /Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx_gpio_driver.h \
+ /Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx.h
+/Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx.h:
+/Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx_gpio_driver.h:
+/Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx.h:
---
+++ embedded/c/udemy/MCU1/stm32f3xx_drivers/Debug/drivers/Src/stm32f303xx_gpio_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f303xx_gpio_driver.o: \
+ ../drivers/Src/stm32f303xx_gpio_driver.c \
+ /Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx_gpio_driver.h \
+ /Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx.h \
+ /Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx_gpio_driver.h
+/Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx_gpio_driver.h:
+/Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx.h:
+/Users/kana/Projects/Education/education/embedded/c/udemy/MCU1/stm32f3xx_drivers/drivers/Inc/stm32f303xx_gpio_driver.h:
